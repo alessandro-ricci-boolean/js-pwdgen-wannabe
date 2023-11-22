7d09bf3f1045ccaceea8b7e998bfddea5081241e
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-22 16:10:32 +0100

    added pw generator

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-let nome = prompt("Inserisci il tuo nome");
+let name = prompt("Inserisci il tuo nome");
 console.log(nome);
 
-let cognome = prompt("Inserisci il tuo cognome");
+let surname = prompt("Inserisci il tuo cognome");
 console.log(cognome);
 
-let colore = prompt("Inserisci il tuo colore preferito");
+let color = prompt("Inserisci il tuo colore preferito");
 console.log(colore);
 
-document.writeln("nomecognomecolorepreferito23")
+let password = name + surname + color + 23
+console.log(password);
